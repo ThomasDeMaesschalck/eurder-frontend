c5204b7344a3040d4449327d04bcf76d4e65025f
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-02 22:36:33 +0100

    Refactored stock status code.

--- src/app/pipes/stock-status.pipe.ts
+++ src/app/pipes/stock-status.pipe.ts
@@ -8,10 +8,9 @@ export class StockStatusPipe implements PipeTransform {
   transform(stockUrgency: string): string {
 
     switch (stockUrgency) {
-      case 'STOCK_HIGH': return 'High';
-      case 'STOCK_LOW': return 'Low'
-      case 'STOCK_MEDIUM': return 'Medium'
-      default: return 'Empty';
+      case 'STOCK_HIGH': return 'high';
+      case 'STOCK_MEDIUM': return 'medium'
+      default: return 'low';
     }
   }
 
