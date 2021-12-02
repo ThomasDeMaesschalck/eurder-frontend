c091db7a7e26af0414db2b8811b7e1af0af12b07
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-02 16:36:48 +0100

    Story IT4: Create Item - Length counter completed.

--- src/app/item/item-edit/item-edit.component.ts
+++ src/app/item/item-edit/item-edit.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ItemService} from "../../services/item.service";
 import {ActivatedRoute, Router} from "@angular/router";
-import {of} from "rxjs";
-import { map, switchMap } from 'rxjs/operators';
+import {max, of} from "rxjs";
+import {map, switchMap} from 'rxjs/operators';
 import {Item} from "../../models/Item";
 
 @Component({
@@ -13,41 +13,56 @@ import {Item} from "../../models/Item";
 
 export class ItemEditComponent implements OnInit {
 
-   id: string | undefined;
-   item!: Item | undefined;
+  id: string | undefined;
+  item!: Item | undefined;
 
-  constructor( private route: ActivatedRoute,
-               private router: Router,
-               private itemService: ItemService) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private itemService: ItemService) {
+  }
 
   ngOnInit(): void {
     this.route.params
-    .pipe(
-      map(p => p['id']),
-      switchMap(id => {
-        if (id === 'new') {
-          // @ts-ignore
-          this.item = {};
-          return of(this.item); }
-        return this.itemService.getById(id);
-      })
-    )
-.subscribe(item => {
-    this.item = item;
-  });
-}
+      .pipe(
+        map(p => p['id']),
+        switchMap(id => {
+          if (id === 'new') {
+            this.item = <Item>{
+              name: '',
+              description: '',
+              price: 0,
+              amountOfStock: 0
+            };
+            return of(this.item);
+          }
+          return this.itemService.getById(id);
+        })
+      )
+      .subscribe(item => {
+        this.item = item;
+      });
+  }
 
   save() {
     this.itemService.save(this.item!).subscribe(
       itemFromBackend => {
         this.item = itemFromBackend;
-          this.router.navigate(['items', this.item.id]);
-        }
+        this.router.navigate(['items', this.item.id]);
+      }
     );
   }
 
 
-cancel() {
+  cancel() {
     this.router.navigate(['']);
+  }
+
+  countCharactersLeft(input: string): number {
+    const maxChars = 255;
+
+    if (input.length === maxChars) {
+      return 0;
+    }
+    return maxChars - input.length;
   }
 }
