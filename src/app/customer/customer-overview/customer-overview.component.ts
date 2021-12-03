42fa2c88128c840940b2e1c982a4884ac5d59dbc
Thomas De Maesschalck <58608713+ThomasDeMaesschalck@users.noreply.github.com>
2021-12-03 10:21:49 +0100

    Story CU1: Customer overview completed.

--- src/app/customer/customer-overview/customer-overview.component.ts
+++ src/app/customer/customer-overview/customer-overview.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Item} from "../../models/Item";
+import {ItemService} from "../../services/item.service";
+import {CustomerService} from "../../services/customer.service";
+import {Customer} from "../../models/Customer";
 
 @Component({
   selector: 'app-customer-overview',
@@ -7,9 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerOverviewComponent implements OnInit {
 
-  constructor() { }
+  customers: Customer[] = [];
+
+  constructor(private customerService: CustomerService) {
+  }
 
   ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  getCustomers(): void {
+    this.customerService.getCustomers()
+      .subscribe(customers => this.customers = customers);
   }
 
 }
